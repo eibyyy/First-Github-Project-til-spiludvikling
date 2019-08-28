c64e03cdee7e06375644e81e46470a9d195b03a7
Eibyyy <eibyfrederik@gmail.com>
2019-08-28 15:05:37 +0200

    Added Unity Project

---
+++ Demo/Assets/Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    private float forceMod = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hvis space bar er trykket ned denne frame...
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        // Unity får fat i figuren, og får giver den krafter til at kunne håbe impulst.s
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forceMod, ForceMode2D.Impulse);
+        }
+    }
+}
